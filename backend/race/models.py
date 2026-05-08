0a0393b8ed8a12bf859f3f272560883c91fea720
DinisSL <dinissantanalopes@gmail.com>
2026-05-08 11:01:55 +0100

    Mais models

--- backend/race/models.py
+++ backend/race/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Race(models.Model):
     name = models.CharField(max_length=200)
@@ -6,3 +7,26 @@ class Race(models.Model):
 
     def __str__(self):
         return self.name
+
+class Runner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    adminComment = models.CharField(max_length=200)
+    classification = models.IntegerField()
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
+
+class Voluntary(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    phoneNumber = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
+
+
+
+
+
+
+class Tarefas(models.Model):
+    nae
