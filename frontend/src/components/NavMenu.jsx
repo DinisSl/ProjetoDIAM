9a7b7fdbd9139032b7731e470094d4aec4162f63
DinisSL <dinissantanalopes@gmail.com>
2026-05-08 21:57:02 +0100

    NavMenu rudimentar

---
+++ frontend/src/components/NavMenu.jsx
@@ -0,0 +1,34 @@
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle,} from "@/components/ui/navigation-menu";
+import { Link } from "react-router-dom";
+
+const NavMenu = () => {
+    return (
+        <div>
+          <NavigationMenu>
+            <NavigationMenuList>
+
+              <NavigationMenuItem>
+                  <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                      <Link to="/login">Login</Link>
+                  </NavigationMenuLink>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                  <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                      <Link to="/loginmanager">Login Manager</Link>
+                  </NavigationMenuLink>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                  <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                      <Link to="/signup">Sign Up</Link>
+                  </NavigationMenuLink>
+              </NavigationMenuItem>
+
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+    )
+}
+
+export default NavMenu;
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -3,11 +3,13 @@ import Footer from "../components/Footer.jsx";
 import Counter from "../components/Counter.jsx";
 import LoginManager from "@/components/LoginManager.jsx";
 import Description from "@/components/Description.jsx";
+import NavMenu from "@/components/NavMenu.jsx";
 
 function Homepage() {
   return (
     <>
       <Header/>
+      <NavMenu/>
       <LoginManager/>
       <Counter/>
       <Description/>
