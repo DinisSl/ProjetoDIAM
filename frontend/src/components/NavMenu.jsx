eda53685fead4920cc978f7d8ddc201b3c57ff62
DinisSL <dinissantanalopes@gmail.com>
2026-05-15 10:53:11 +0100

    Counter a funcionar

--- frontend/src/components/NavMenu.jsx
+++ frontend/src/components/NavMenu.jsx
@@ -4,14 +4,47 @@ import {
   NavigationMenuTrigger, navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { Link } from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const NavMenu = () => {
+  const URL_RACES = 'http://localhost:8000/race/api/races/';
+  const [races, setRaces] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get(URL_RACES)
+      .then((response) => {
+        setRaces(response.data)
+      }
+      )
+      .catch(() =>
+        console.log('erro ao ir buscar as corridas')
+      );
+  }, []);
 
     return (
         <div>
           <NavigationMenu>
             <NavigationMenuList className="gap-2">
+
+              <NavigationMenuItem className="border rounded-lg">
+
+                <NavigationMenuTrigger>Corridas</NavigationMenuTrigger>
+
+                  <NavigationMenuContent>
+                    {races.map((race) => (
+                      <NavigationMenuLink
+                        key={race.id}
+                        asChild
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        <Link to={`/race/${race.id}`}>{race.name}</Link>
+                      </NavigationMenuLink>
+                    ))}
+                  </NavigationMenuContent>
+
+              </NavigationMenuItem>
 
               <NavigationMenuItem className="border rounded-lg">
                   <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
